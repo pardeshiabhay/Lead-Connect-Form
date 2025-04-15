0ba75154c985dd37e4417537f889a4a994ee5f4b
pardeshiabhay <pardeshiabhay999@gmail.com>
2025-04-16 02:38:53 +0530

    reCaptcha Validations

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-function beforesubmit(){
-let outputdate = document.querySelectorAll(".outputdate");
-let inputdate = document.querySelectorAll(".inputdate");
+let capthachecked = false;
+function beforesubmit(event){
+    if(capthachecked){
+    let outputdate = document.querySelectorAll(".outputdate");
+    let inputdate = document.querySelectorAll(".inputdate");
 
-console.log("inputdate.value",value);  //string --> date(en_IN)
+    console.log("inputdate.value",value);  //string --> date(en_IN)
 
-let formatDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-outputdate.value = formatDate;
+    let formatDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatDate;      
+    }else{
+        alert("Please check the reCAPTCHA box to submit the lead");
+        event.preventDefault();
 
+    }
 }
 function timestamp() { 
 var response = document.getElementById("g-recaptcha-response"); 
 if (response == null || response.value.trim() == "") 
     {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
         elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
-        }
-        } setInterval(timestamp, 500);
+    }
+ } 
+setInterval(timestamp, 500);
+
+function capthchasuccess(){
+    capthachecked = true;
+}
+
